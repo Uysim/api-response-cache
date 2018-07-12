2545dc1795a0fd9bcc02bd3e3a0e5299e8cf5f43
Uysim Ty <uysimty@gmail.com>
2018-07-12 10:24:31 +0700

    write cache only when get request

--- lib/api_response_cache/api_cache_handler.rb
+++ lib/api_response_cache/api_cache_handler.rb
@@ -14,7 +14,7 @@ module ApiResponseCache
           render_cached_response
         else
           yield
-          @response_cache.write_cache(controller.response)
+          @response_cache.write_cache(controller.response) if @request.get?
         end
       end
 
@@ -25,23 +25,27 @@ module ApiResponseCache
       end
 
       def log_info
-        processor     = @processor_name.blue
-        responder     = Rainbow('API Response Cache').green
+        processor       = "#{@controller.class.name}##{@controller.action_name}".blue
+        responder       = Rainbow('API Response Cache').green
         Rails.logger.info "=== #{processor} response by #{responder} ==="
       end
 
       def init(controller)
         @controller         = controller
         @request            = controller.request
-        @processor_name     = "#{@controller.class.name}##{@controller.action_name}"
         @response_cache     = ResponseCache.new(cache_path, @expires_in)
       end
 
       def cache_path
-        @cache_path = "api-response-cache/#{@processor_name || @options[:cache_path]}"
+        @cache_path = "api-response-cache"
 
-        if ApiResponseCache.configuration.refresh_by_request_params?
-          @cache_path = "#{@cache_path}#{@request.fullpath}"
+        if @options[:cache_path].present?
+          @cache_path = "#{@cache_path}/#{@options[:cache_path]}"
+        elsif ApiResponseCache.configuration.refresh_by_request_params?
+          @cache_path = "#{@cache_path}/#{@request.fullpath}"
+        else
+          path_only   = @request.fullpath.split('?').first
+          @cache_path = "#{@cache_path}/#{path_only}"
         end
 
         if ApiResponseCache.configuration.cache_by_headers.present?
